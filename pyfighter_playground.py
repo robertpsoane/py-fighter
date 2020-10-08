06401b9b75bdb11bc726dda770dd16f6f80b56ff
robertpsoane <robertsoane101@gmail.com>
2020-10-08 21:19:59 +0100

    Added playground for testing objects

---
+++ pyfighter_playground.py
@@ -0,0 +1,62 @@
+'''
+Pyfighter Playground
+
+A script for testing objects in isolation.
+'''
+
+### Library Imports
+import pygame
+import json
+
+
+### Important Game Variables
+# Colours
+black = 0, 0, 0
+white = 255, 255, 255
+red = 255, 0, 0
+green = 0, 255, 0
+blue = 0, 0, 255
+
+
+# Setting up screen variables
+screen_height, screen_width = 500, 700
+game_name = 'PyFighter (working title)'
+fps = 60
+
+
+### Setting up Screen and clock
+game_screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption(game_name)
+clock = pygame.time.Clock()
+
+'''
+Initialise your objects here
+'''
+
+
+### Setting up game loop
+run_me = True
+
+while run_me:
+    # Limit frame rate
+    clock.tick(fps)
+
+    # Get/action events
+    for event in pygame.event.get():
+        
+
+        if event.type == pygame.QUIT:
+            # Detecting user pressing quit button, if X pressed,
+            # break loop and quit screen.
+            run_me = False
+        
+        
+
+    # Refresh screen
+    game_screen.fill(black)
+
+    ### Code to re-display items on screen will go here
+    '''
+    Blit your objects to the screen here
+    '''
+
