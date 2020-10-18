b4bce0a69b3307cf0354d2a4dbd14069a81d3242
robertpsoane <robertsoane101@gmail.com>
2020-10-18 11:53:15 +0100

    Built DisplayString class

---
+++ classes/menu.py
@@ -0,0 +1,12 @@
+''' Menu Class
+
+Class to create menu in pygame.  Used initially for start menu.  Can be 
+extended if required to create pause and other menues throughout the game.
+
+'''
+
+import pygame
+
+class Menu:
+    def __init__():
+        pass
