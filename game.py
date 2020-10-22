92aa648600221f3d48feb873bb27c58457557738
RokasDan <rokas108@gmail.com>
2020-10-22 22:57:29 +0100

    Added a temporary Player.

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ A Steampunk 2D streetfighting game built with PyGame.
 
 [ More details to go here before release :), big it up and that! ]
 
-Produced as an MSc Computer Science project by R. Soane, 
+Produced as an MSc Computer Science project by R. Soane,
 S. Mistrey and R. Danevicius
 '''
 
@@ -13,48 +13,54 @@ import pygame
 import json
 from classes.displaystring import DisplayString
 
+######################### DELETE WHEN PLAYER CLASS IS MADE ###################
+player_image = pygame.image.load('graphics/char_idle/idler1.png')
+##############################################################################
+
 def pyfighterGame():
-    ### Important Game Variables from JSON
-    with open('json/config.JSON') as config_file:
-        config = json.load(config_file)
+	### Important Game Variables from JSON
+	with open('json/config.JSON') as config_file:
+		config = json.load(config_file)
 
-    # Colour tuples and font sizes
-    colour = config['colour']
-    font_size = config['font_size']
+	# Colour tuples and font sizes
+	colour = config['colour']
+	font_size = config['font_size']
 
-    # Important screen variables
-    screen_width = config['screen_dims'][0]
-    screen_height = config['screen_dims'][1]
-    max_fps = config['max_fps']
-    game_name = config['game_name']
+	# Important screen variables
+	screen_width = config['screen_dims'][0]
+	screen_height = config['screen_dims'][1]
+	max_fps = config['max_fps']
+	game_name = config['game_name']
 
-    ### Setting up Screen and clock
-    game_screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption(game_name)
-    clock = pygame.time.Clock()
+	### Setting up Screen and clock
+	game_screen = pygame.display.set_mode((screen_width, screen_height))
+	pygame.display.set_caption(game_name)
+	clock = pygame.time.Clock()
 
-    ### Setting up game loop
-    run_me = True
+	### Setting up game loop
+	run_me = True
 
-    while run_me:
-        # Limit frame rate
-        clock.tick(max_fps)
+	while run_me:
+		# Limit frame rate
+		clock.tick(max_fps)
 
-        # Get/action events
-        for event in pygame.event.get():
-            
-            if event.type == pygame.QUIT:
-                # Detecting user pressing quit button, if X pressed,
-                # break loop and quit screen.
-                run_me = False
-            
-        # Refresh screen
-        game_screen.fill(colour['blue'])
+		# Get/action events
+		for event in pygame.event.get():
 
-        ### Code to re-display items on screen will go here ###
-        
+			if event.type == pygame.QUIT:
+				# Detecting user pressing quit button, if X pressed,
+				# break loop and quit screen.
+				run_me = False
 
+		# Refresh screen
+		game_screen.fill(colour['blue'])
+
+		### Code to re-display items on screen will go here ###
 
 
-        # Flip to display
-        pygame.display.flip()
+		######################### DELETE WHEN PLAYER CLASS IS MADE ###################
+		game_screen.blit(player_image, (50, 50))
+
+		##############################################################################
+		# Flip to display
+		pygame.display.flip()
