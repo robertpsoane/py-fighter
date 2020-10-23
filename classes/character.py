b9ebd5e4e1fc667254207f208a69d5744ae1e944
robertpsoane <robertsoane101@gmail.com>
2020-10-23 10:43:22 +0100

    Starting on general image processor for character

--- classes/character.py
+++ classes/character.py
@@ -2,7 +2,24 @@
 
 
 '''
+import pygame
 
 class Character:
-    def __init__():
-        pass
+    
+    def setup(self, img_data):
+        self.spritesheet = pygame.image.load(img_data['path'])
+
+    
+        
+    
+    
+    
+    def move(self,direction):
+        if direction == 'l':
+            pass
+        elif direction == 'r':
+            pass
+        elif direction == 'u':
+            pass
+
+    
--- classes/player.py
+++ classes/player.py
@@ -2,7 +2,34 @@
 
 
 '''
+from classes.character import Character
 
-class Player:
-    def __init__():
-        pass
+class Player(Character):
+    def __init__(self):
+        ### Setting up mapping from sprite sheet to images
+        ### This can become a JSON
+        running = {
+            'left': [(0, 0), (0, 1), (0, 2), (0, 3), (0, 0),
+                    (0, 4), (0, 5), (0, 6)],
+            'right': [(2, 0), (2, 1), (2, 2), (2, 3), (2, 0),
+                    (2, 4), (2, 5), (2, 6)]
+        }
+
+        idle = {
+            'left': [(0, 0), (1, 0)],
+            'right': [(2, 0), (3, 0)]
+        }
+        
+        self.image_data = {
+            'running': running,
+            'idle': idle,
+            'path': 'graphics/spritesheets/basic-character.png',
+            'background': (0, 255, 0),
+            'gridsize': (4, 9),
+            'charsize': (32, 32)
+
+        }
+
+        self.setup(self.image_data)
+
+    
