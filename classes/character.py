fa31575254e3712f54129f74db30080c7e491e33
robertpsoane <robertsoane101@gmail.com>
2020-10-23 16:33:34 +0100

    Added left-right move functions

--- classes/character.py
+++ classes/character.py
@@ -1,5 +1,55 @@
 ''' Character Class
 
+Character class for py-fighter game.
+Takes input of character data, screen, x position, and y position.
+
+Has functions to make move.
+
+- character_data takes the form of a Python dictionary with all key components 
+and data for character.  This is usually stored in a JSON. We have decided
+to implement like this as it allows us to add future characters or make 
+significant changes to the characters without having to edit the python code.
+
+character_data follows the following structure:
+
+{
+    "actions": ["running", "idle"],  <-- These lines should not be edited
+    "directions": ["left", "right"], <-- from the preset as changing them may
+                                        cause the game to break
+    "running": {
+        "left": [[0, 0], [0, 1], [0, 2], [0, 3], [0, 0], [0, 4], [0, 5], [0, 6]],
+        "right": [[2, 0], [2, 1], [2, 2], [2, 3], [2, 0], [2, 4], [2, 5], [2, 6]]
+        },  
+
+    "idle": {
+        "left": [[0, 0], [1, 0]],
+        "right": [[2, 0], [3, 0]]
+        },
+
+        ^ These nested lists give the coordinates within the grid (in order) ^
+            of where the images that make up the given action can be found
+            in the sprite sheet.  There should be one dictionary for each action,
+            containing a list for each direction
+
+    "path": "graphics/spritesheets/basic-character.png", <-- File path of the
+    "background": [0, 255, 0], <-- Background colour        spritesheet
+    "gridsize": [4, 9], <-- Grid size of sprite sheet (zero-indexed)
+    "charsize": [32, 32], <-- Size of character in pixels
+    "scaledsize": [128, 128], <-- Size to scale character to in pixels
+    "speed": 1, <-- Speed of character in pixels per frame
+    "gravity": 1, <-- Gravitationaly speed in pixels per frame
+    "refresh": 10, <-- Number of frames between character refresh
+    "initialstate": ["running", "right"] <-- State the character is initially
+                                            spawned in (eg direction facing)
+}
+
+Future Plans:
+- Gravity/falling
+- Jump function
+- Health functionality
+- Attack functionality
+- Recoil functionality
+
 
 '''
 import pygame
@@ -7,13 +57,12 @@ from classes.spritesheet import SpriteSheet
 
 class Character(pygame.sprite.Sprite):
     
-    def __init__(self, screen, x_position, y_position):
+    def __init__(self, character_data, screen, x_position, y_position):
         # Declaring self to be a sprite
         pygame.sprite.Sprite.__init__(self)
 
-        # Assigning self.character_data to local variable character_data
-        # to make code more readable
-        character_data = self.character_data
+        # Assigning character data to self.charactar_data
+        self.character_data = character_data
 
         ### Unpacking some important JSON dictionary into variables
         self.speed = character_data['speed']
@@ -103,13 +152,53 @@ class Character(pygame.sprite.Sprite):
         if self.image_index == n_images:
             self.image_index = 0
     
+    def updateState(self, action, direction):
+        ''' updateState(action, direction)
+        function to update state of character
+        '''
+        self.state = [action, direction]
+
+    def moveX(self, step):
+        ''' moveX(step)
+        Function to move character step pixels in the X direction
+        '''
+        self.position[0] += step
+        self.rect.center = self.position
+
+    def moveY(self, step):
+        ''' moveY(step)
+        Function to move character step pixels in the Y direction.
+        - Note: the y axis is flipped from what we might naturally assume,
+                0 is at the top and not the bottom
+        '''
+        self.position[1] += step
+        self.rect.center = self.position
+
     
-    def move(self,direction):
+    def startMove(self,direction):
+        ''' startMove(direction)
+        Input 'l' or 'r' for horizontal movement, 'u' for jump
+        '''
         if direction == 'l':
-            pass
+            # Moving one speed step left
+            self.moveX(self.speed)
+            self.state = ['running', 'left']
         elif direction == 'r':
-            pass
+            # Moving one speed step right
+            self.moveX(-1 * speed)
+            self.state = ['running', 'right']
         elif direction == 'u':
             pass
+    
+    def stopMove(self):
+        ''' stopMove()
+        Returns state to idle when no longer moving.  Purpose of function is 
+        to stop running animation.
+
+        WILL NEED CHANGING WHEN WEAPONS ARE IMPLEMENTED! Will need to choose 
+        state based on weapon!
+        '''
+        self.state[0] = 'idle'
+    
 
     
--- classes/player.py
+++ classes/player.py
@@ -1,5 +1,11 @@
 ''' Player Class
 
+Player class of py-fighter game.
+Inherits from Character.  Initialises by loading basic_character JSON and 
+initialising as Character.
+
+This is distinct from the NPC class, as the NPC class will have a basic AI
+built in to attack the player, whereas the player is fairly simple.
 
 '''
 import json
@@ -9,9 +15,9 @@ class Player(Character):
     def __init__(self, screen, x_position, y_position):
         # Loading player data json, and converitng to python dictionary
         with open('json/basic_character.JSON') as player_json:
-            self.character_data = json.load(player_json)
+            character_data = json.load(player_json)
 
         # Initialising Character class
-        Character.__init__(self, screen, x_position, y_position)
+        Character.__init__(self, character_data, screen, x_position, y_position)
 
     
