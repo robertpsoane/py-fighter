bc85d3e0aeb732b4df8112500196d4226da7450b
robertpsoane <robertsoane101@gmail.com>
2020-11-01 23:39:26 +0000

    Added placeholder enemy

--- classes/npc.py
+++ classes/npc.py
@@ -4,5 +4,10 @@
 '''
 
 class NPC:
-    def __init__():
-        pass
+    def __init__(self, screen, x_position, y_position):
+        # Loading player data json, and converitng to python dictionary
+        with open('json/basic_enemy.JSON') as player_json:
+            character_data = json.load(player_json)
+
+        # Initialising Character class
+        Character.__init__(self, character_data, screen, x_position, y_position)
