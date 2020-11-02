36b52bc9968d424e3369bdfa599b338c462d2949
robertpsoane <robertsoane101@gmail.com>
2020-11-02 14:23:36 +0000

    updates to npc

--- classes/npc.py
+++ classes/npc.py
@@ -7,10 +7,14 @@ import json
 from classes.character import Character
 
 class NPC(Character):
-    def __init__(self, screen, x_position, y_position):
+    def __init__(self, screen, x_position, y_position, type = 'basic'):
         # Loading player data json, and converitng to python dictionary
-        with open('json/basic_enemy.JSON') as player_json:
-            character_data = json.load(player_json)
-
+        json_location = 'json/{}_enemy.JSON'
+        try:
+            with open(json_location) as player_json:
+                character_data = json.load(player_json)
+        except:
+            err_string = "Bad Enemy Name.  Please input an enemy which has a corresponding JSON"
+            raise Exception(err_string)
         # Initialising Character class
         Character.__init__(self, character_data, screen, x_position, y_position)
