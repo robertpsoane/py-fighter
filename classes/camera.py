e7237598727e3012db189684c14fd87eb3935a5b
RokasDan <rokas108@gmail.com>
2020-11-17 00:42:33 +0000

    Camera fixed on the player

--- classes/camera.py
+++ classes/camera.py
@@ -3,10 +3,11 @@
 '''
 
 class Camera:
-    def __init__(self, x=1, y=0):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
         self.sprites = []
+
 
     def addMap(self, map_obj):
         self.map = map_obj
@@ -15,8 +16,13 @@ class Camera:
         self.sprites.append(sprite)
 
     def scroll(self):
+
         for tile in self.map.map_group:
             tile.rect.centerx -= self.x
 
         for sprite in self.sprites:
             sprite.rect.centerx -= self.x
+
+        self.player = self.sprites[0]
+        self.x += (self.player.rect.centerx-self.x-200)
+
