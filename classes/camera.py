a6bcdf85c7e7fedb817005845d29bb60363be535
RokasDan <rokas108@gmail.com>
2020-11-17 23:44:49 +0000

    added camera move function to check for boundries

--- classes/camera.py
+++ classes/camera.py
@@ -2,27 +2,54 @@
 @author: Rokas Danevicius (unless stated otherwise)
 '''
 
+
 class Camera:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
         self.sprites = []
 
-
     def addMap(self, map_obj):
         self.map = map_obj
 
     def add(self, sprite):
         self.sprites.append(sprite)
+        self.init_player = self.sprites[0]
+        self.init_position = self.init_player.position[0]
+
+    def cameraTrack(self):
+        self.init_position += 2
+
+    def cameraMove(self, direction):
+        if direction == 'r':
+            self.cameraTrack()
+        if direction == "l":
+            self.init_position += -1 * 2
 
     def scroll(self):
 
-        for tile in self.map.map_group:
-            tile.rect.centerx -= self.x
+        print(self.init_position)
 
-        for sprite in self.sprites:
-            sprite.rect.centerx -= self.x
+        if self.init_position <= 100 or self.init_position >= 800:
 
-        self.player = self.sprites[0]
-        self.x += (self.player.rect.centerx-self.x-200)
+            for tile in self.map.map_group:
+                tile.rect.centerx -= self.x
+
+            for sprite in self.sprites:
+                sprite.rect.centerx -= self.x
+
+            self.player = self.sprites[0]
+            self.x += 0
+
+        else:
+            for tile in self.map.map_group:
+                tile.rect.centerx -= self.x
+
+            for sprite in self.sprites:
+                sprite.rect.centerx -= self.x
+
+            self.player = self.sprites[0]
+            self.x += (self.player.rect.centerx - self.x - 200)
+
+
 
