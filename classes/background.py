7c223cb7c31e991c1f676d77055b6ee48b448e7f
RokasDan <rokas108@gmail.com>
2020-11-24 21:13:38 +0000

    documented background class

--- classes/background.py
+++ classes/background.py
@@ -1,8 +1,25 @@
+'''
+@author: Rokas Danevicius (unless stated otherwise)
+The Class creates and blits a background which is then capable of parallax scrolling once
+camera Class starts updating the x values of each background object.
+'''
+
+
 import pygame
+
+# Creating information for each object which will be displayed on the background.
+
+# The information is stored in a list of lists.
+# as an example [increment, [x_position, y_position]].
+# First parameter stores the increment of the scroll.
+# The increment will be used to control the speed of the scroll for that particular object.
+# x_position is where the object will be blited on x axes.
+# y_position is where the object will be blited on y axes.
 
 background_objects = [[0.25, [0, 0]], [0.25, [256, 0]], [0.5, [0, 64]], [0.5, [192, 64]],
                       [0.5, [384, 64]], [0.5, [576, 64]]]
 
+# Load the images used by the objects also scale the images to fit the size of the map.
 back1 = pygame.image.load('graphics/background_sprite/back_1.png')
 back1 = pygame.transform.scale(back1, (192, 192))
 
@@ -11,19 +28,34 @@ super_back1 = pygame.transform.scale(super_back1, (128 * 2, 128 * 2))
 
 
 class Background:
+    """ Class reiterates through a matrix containing the increment and x/y positions of each object
+    and blits an image using those values."""
 
-    def __init__(self, screen, objects=0):
-        self.objects = objects
+
+    def __init__(self, screen):
+        # Variable which temporarily stores information about a single object
+        # when iteration is happening.
+        self.objects = []
+        # Gets the surface to which the background will be displayed.
         self.display = screen
+        # Variable which gets updated with x scroll value from the Camera class.
         self.move = 0
 
     def displayQ(self):
+        ''' Method which blits the background to the display'''
 
-
-
+        # Reiterating through "background_objects" matrix to get all of the values for
+        # each individual background object.
         for background_object in background_objects:
+
+            # x value of each object gets updated by the camera scroll stored in self.move
+            # value when the camera scroll() method is active.
+            # The speed of the scrolling gets determined by the increment which is
+            # stored in background_object[0]
             self.objects = (background_object[1][0] - self.move * background_object[0],
                             background_object[1][1])
+
+            # TODO this will change to random choice of background images.
             if background_object[0] == 0.5:
                 self.display.blit(back1, self.objects)
             if background_object[0] == 0.25:
--- classes/camera.py
+++ classes/camera.py
@@ -1,54 +1,84 @@
 '''
 @author: Rokas Danevicius (unless stated otherwise)
+The class which creates a camera view for the game which fallows the player.
 '''
 
 
 class Camera:
+    '''
+    The class which creates a camera view for the game which fallows the player on the x axes.
+    The class does so by updating the x value of each objects drawn of the game_display. The
+    position of the camera view is determined by the position of player character position on x
+    axes. The class is also able to fallow the player on y axes if one wishes to.
+    '''
+
+
     def __init__(self, x=0, y=0):
+
+        # x and y values used to update the x or y values of other blited objects.
         self.x = x
         self.y = y
+
+        # List used to save the name of character Class instances.
         self.sprites = []
+        # Create a variable to follow Players position in the map when camera is not working.
         self.world_x = x
 
     def addBack(self, background_object):
+        ''' Access the variables of background class instance.'''
         self.background_scroll = background_object
 
     def addMap(self, map_obj):
+        ''' Access the variables of Map class instance.'''
         self.map = map_obj
 
     def add(self, sprite):
+        ''' Access the variables of all Character class instance.'''
         self.sprites.append(sprite)
-        self.init_player = self.sprites[0]
-        self.init_position = self.init_player.position[0]
+        # self.init_player = self.sprites[0]
+        # self.init_position = self.init_player.position[0]
 
 # TODO rename the init_position
 
     def scroll(self):
+        ''' Method which updates the x values of objects which are blited to game_display'''
 
+        # Creates a variable for the visible map width.
         map_width = self.map.dims[0] - 400
 
+        # Creating boarders for the camera view.
+        # Stops the camera moving if the camera hits the left side of the level.
         if self.world_x > map_width:
             self.x = 0
             self.world_x = map_width
+
+        # Stops the camera moving if the camera hits the right side of the level.
         elif self.world_x < 0:
             self.x = 0
             self.world_x = 0
+
+        # Camera moves, the values of x values of objects that are not the player get updated.
         else:
 
+            # Moves the background objects. Updates their x value.
             self.background_scroll.move += self.x
 
+            # Moves the Map sprite. Updates its x value.
             for tile in self.map.map_group:
                 tile.rect.centerx -= self.x
 
+            # Moves the player and NPC sprites. Updates their x values.
             for sprite in self.sprites:
                 sprite.rect.centerx -= self.x
 
+            # Locks the camera to the player sprite.
             self.player = self.sprites[0]
 
             self.x = (self.player.rect.centerx - 400 / 2)
 
-
             self.world_x += self.x
+
+
 
 
 
