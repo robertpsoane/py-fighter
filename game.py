09fd91f5641e910d6baec0c64a90506571ab861a
Robert Soane <64409800+robertpsoane@users.noreply.github.com>
2020-11-24 16:23:46 +0000

    Update game.py
    Updating to prevetn conflict

--- game.py
+++ game.py
@@ -11,7 +11,6 @@ S. Mistrey and R. Danevicius
 ### Library Imports
 import pygame
 import json
-
 from classes.displaystring import DisplayString
 
 ### THESE IMPORTS ARE NEEDED IN CONTROLLER
@@ -19,7 +18,6 @@ from classes.background import Background
 from classes.player import Player
 from classes.npc import NPC
 from classes.controller import Controller
-
 
 def pyfighterGame():
     ### Important Game Variables from JSON
@@ -39,12 +37,12 @@ def pyfighterGame():
 
     ### Setting up Screen and clock
     game_screen = pygame.display.set_mode((screen_width, screen_height))
-    game_display = pygame.Surface((screen_width / 2, screen_height / 2))  # surface on which we blit images
+    game_display = pygame.Surface((screen_width, screen_height))  # surface on which we blit images
     pygame.display.set_caption(game_name)
     clock = pygame.time.Clock()
 
     ######## Will go in controller init!
-    game_controller = Controller(game_display, game_screen, screen_dims)
+    game_controller = Controller(game_screen, screen_dims)
     game_controller.generateMap()
 
     ########
@@ -70,19 +68,19 @@ def pyfighterGame():
 
             ######################################################
         # Refresh screen
-        game_display.fill(colour['main'])
+        game_screen.fill(colour['blue'])
 
         ### Code to re-display items on screen will go here ###
 
         game_controller.display()
 
-        # scaled_surf = pygame.transform.scale(game_display, screen_dims)
-        # game_screen.blit(scaled_surf, (0, 0))
-        # game_screen.blit(scaled_surf, (0, 0))
+        # Blits the scaled game_display to game_screen, move to controller when ready #
+        #scaled_surf = pygame.transform.scale(game_display, screen_dims)
+        #game_screen.blit(scaled_surf, (0, 0))
         ##############################################################################
         # Flip to display
         pygame.display.flip()
 
-
-#### COMMENT BELOW OUT - only here for testing purposes now :)
-pyfighterGame()
+# Used for testing - can run game direct from this file, bypassing the start menu
+if __name__ == '__main__':
+    pyfighterGame()
